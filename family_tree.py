5b303398c975abce979a8d57e65818fc49711206
rvlb-19 <renatovlbarros19@gmail.com>
2020-07-11 14:18:49 -0300

    Update marriage -> parentage terminology

--- family_tree.py
+++ family_tree.py
@@ -13,8 +13,8 @@ class FamilyTree:
         default_node_color="green",
     ):
         self.nodes = set()
-        self.persons_marriages_mapping = defaultdict(set)
-        self.marriage_edges = defaultdict(set)
+        self.persons_parentages_mapping = defaultdict(set)
+        self.parentage_edges = defaultdict(set)
         self.children_edges = defaultdict(set)
         self.labels = dict()
 
@@ -24,28 +24,29 @@ class FamilyTree:
         self.default_node_color = default_node_color
 
     def _generate_nodes_and_edges(
-        self, persons, person_label_key, person_parents_key, person_node_size=300, marriage_node_size=10
+        self, persons, person_label_key, person_parents_key, person_node_size=300, parentage_node_size=10
     ):
         for person in persons:
             person_label_value = person[person_label_key]
             self.nodes.add((person_label_value, person_node_size))
             self.labels[person_label_value] = person_label_value
-            # Add parents marriage edge
+            # Add parentage edge
             if person_parents_key in person.keys():
                 parents = sorted(person[person_parents_key])
-                # Create marriage hash
-                marriage_hash = uuid.uuid5(uuid.NAMESPACE_OID, " ".join(parents))
+                # Create parentage hash
+                parentage_hash = uuid.uuid5(uuid.NAMESPACE_OID, " ".join(parents))
                 for p in parents:
                     self.nodes.add((p, person_node_size))
-                    self.marriage_edges[marriage_hash].add((p, marriage_hash))
-                    self.persons_marriages_mapping[p].add(marriage_hash)
+                    self.parentage_edges[parentage_hash].add((p, parentage_hash))
+                    self.persons_parentages_mapping[p].add(parentage_hash)
                     self.labels[p] = p
-                # Add marriage nodes to set
+                # Add parents nodes to set
                 self.nodes.update((p, person_node_size) for p in parents)
-                self.nodes.add((marriage_hash, marriage_node_size))
+                # Add parentage node to set
+                self.nodes.add((parentage_hash, parentage_node_size))
                 # Add child node to set
                 self.nodes.add((person_label_value, person_node_size))
-                self.children_edges[marriage_hash].add((marriage_hash, person_label_value))
+                self.children_edges[parentage_hash].add((parentage_hash, person_label_value))
 
     def _get_node_color(self, node):
         for (color_lambda, color) in self.color_mapping_list:
@@ -62,17 +63,17 @@ class FamilyTree:
         t = (abx*acx + aby*acy) / (abx*abx + aby*aby)
         return (a[0] + t*abx, a[1] + t*aby)
 
-    def _adjust_marriage_nodes_positions(self, positions):
-        for marriage_hash, edges in self.marriage_edges.items():
+    def _adjust_parentage_nodes_positions(self, positions):
+        for parentage_hash, edges in self.parentage_edges.items():
             edges_list = [edge[0] for edge in edges]
-            marriage_member_0 = positions[edges_list[0]]
-            marriage_member_1 = positions[edges_list[1]]
+            parentage_member_0 = positions[edges_list[0]]
+            parentage_member_1 = positions[edges_list[1]]
             projection = self._orthogonal_projection(
-                marriage_member_0,
-                marriage_member_1,
-                positions[marriage_hash],
+                parentage_member_0,
+                parentage_member_1,
+                positions[parentage_hash],
             )
-            positions[marriage_hash] = projection
+            positions[parentage_hash] = projection
         return positions
 
     def get_descendants_from(self, current, descendants_set=None):
@@ -81,26 +82,26 @@ class FamilyTree:
         if current in descendants_set:
             return descendants_set
         descendants_set.add(current)
-        for marriage in self.persons_marriages_mapping[current]:
-            for child_edge in self.children_edges[marriage]:
+        for parentage_hash in self.persons_parentages_mapping[current]:
+            for child_edge in self.children_edges[parentage_hash]:
                 descendants_set.update(
                     self.get_descendants_from(child_edge[1], descendants_set=descendants_set)
                 )
         return descendants_set
 
     def plot_graph(self, root_node=None, label_offset_x=0, label_offset_y=0):
-        marriage_edges_set = set()
-        for edges in self.marriage_edges.values():
-            marriage_edges_set.update(edges)
+        parentage_edges_set = set()
+        for edges in self.parentage_edges.values():
+            parentage_edges_set.update(edges)
 
         children_edges_set = set()
         for edges in self.children_edges.values():
             children_edges_set.update(edges)
 
         G = nx.DiGraph()
-        G.add_edges_from(children_edges_set | marriage_edges_set)
+        G.add_edges_from(children_edges_set | parentage_edges_set)
 
-        positions = self._adjust_marriage_nodes_positions(
+        positions = self._adjust_parentage_nodes_positions(
             nx.nx_agraph.pygraphviz_layout(G, root=root_node)
         )
 
@@ -118,7 +119,7 @@ class FamilyTree:
             font_size=8,
             font_weight="bold",
         )
-        nx.draw_networkx_edges(G, pos=positions, edgelist=marriage_edges_set, edge_color="blue", arrows=False)
+        nx.draw_networkx_edges(G, pos=positions, edgelist=parentage_edges_set, edge_color="blue", arrows=False)
         nx.draw_networkx_edges(G, pos=positions, edgelist=children_edges_set, edge_color="blue", arrows=True)
 
         plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
--- main.py
+++ main.py
@@ -7,10 +7,20 @@ with open("dark_dataset.json", mode="r") as f:
 ft = FamilyTree(dataset["characters"])
 
 ft.color_mapping_list = [
-    (lambda n: n[0] in ft.marriage_edges.keys(), "blue"),
+    (lambda n: n[0] in ft.parentage_edges.keys(), "blue"),
+    # This condition deals with descendants from the knot.Why start from Silja?
+    # We could start from Jonas & Martha's child, but them we would never be able
+    # to reach Silja and her descendents, because both her parents exist in the
+    # Origin World. However, because she was conceived through time travelling,
+    # which was never invented in Origin World, she can't exist there too.
     (lambda n: n[0] in ft.get_descendants_from("Silja Tiedemann"), "red"),
+    # Bartosz is a special case because Regina exists in the Origin World
+    # and Boris Niewald probably does too. However, both them only met
+    # while Regina was being bullied by Katharina and Ulrich, the latter
+    # which doesn't exist in the Origin World. As we can't conclude his
+    # true status, he gets a special color here
     (lambda n: n[0] == "Bartosz Tiedemann", "yellow"),
 ]
 
-root_node = list(ft.persons_marriages_mapping["Silja Tiedemann"])[0]
-ft.plot_graph(root_node=root_node, label_offset_y=25)
+root_node = list(ft.persons_parentages_mapping["Silja Tiedemann"])[0]
+ft.plot_graph(root_node=root_node, label_offset_y=30)
